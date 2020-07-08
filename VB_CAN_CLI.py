33abe9cfe39d23a2d00a30e8fc09809db988b613
greencrow <slonik256@yandex.ru>
2020-07-08 16:47:29 +0300

    initial

---
+++ VB_CAN_CLI.py
@@ -0,0 +1,60 @@
+import argparse
+ 
+from test import main, main1, main2, main3
+       
+def RT_set(params):
+    print(len(params))
+    if len(params) != 5:
+        print("Noncorrect number of parameters given! 5 parameters needed")
+    print("Realtime mode choosen")
+    main(params)
+   
+def Reg_set(params):
+    print(len(params))
+    if len(params) != 2:
+        print("Noncorrect number of parameters given! 2 parameters needed")
+    print("U nas tut registry!")
+    main1(params)
+   
+def Id(params):
+    if len(params) != 1:
+        print("Noncorrect number of parameters given! 1 parameter needed")
+    print("U nas tut Id!")
+    main2(params)
+   
+def RF(params):
+    if len(params) < 2:
+        print("Noncorrect number of parameters given! At least 2 needed")
+    print("U nas tut RF!")
+    main3(params)
+   
+def int_or_float(value):
+    try:
+        return int(value, 0) # handle hex int format
+    except:
+        return float(value) # or just float
+ 
+parser = argparse.ArgumentParser(description='Simple CAN communication script')
+ 
+parser.add_argument('-RT', dest='process', action='store_const',
+                    const=RT_set,
+                    help='specifies RT communication')
+ 
+parser.add_argument('-Reg', dest='process', action='store_const',
+                    const=Reg_set,
+                    help='specifies Reg writing')
+ 
+parser.add_argument('-Id', dest='process', action='store_const',
+                    const=Id,
+                    help='specifies device Id')
+ 
+ 
+parser.add_argument('-RF', dest='process', action='store_const',
+                    const=RF,
+                    help='specifies reg fields access')
+ 
+parser.add_argument('variables', type=int_or_float, nargs='*',
+                    help='floats to send to CAN device')
+ 
+args = parser.parse_args() #values extracted from command line
+args.process(args.variables) #callback is callbacked. Fuck you!
---
+++ test.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 10 22:33:30 2020
+ 
+@author: pi
+"""
+ 
+import os
+import can
+import time
+import struct
+import numpy as np
+
+dev_id = 1
+ 
+def main(params):
+    os.system('sudo ifconfig can0 down')
+    os.system('sudo ip link set can0 up type can bitrate 1000000   dbitrate 1000000 restart-ms 1000 loopback on berr-reporting on fd on')
+   
+    can0 = can.interface.Bus(channel = 'can0', bustype='socketcan_ctypes', bitrate=1000000, data_bitrate=8000000, fd=True)
+   
+    message_array = bytearray(struct.pack('5f', *params))
+    msg_tx = can.Message(arbitration_id=0x20*dev_id, dlc=20, data= message_array, is_fd=True, extended_id=False)
+   
+    can0.send(msg_tx, 0.5)
+   
+    msg_rx = can0.recv()
+ 
+    while 1:
+ 
+        message_array = bytearray(struct.pack('5f', *params))
+        msg_tx = can.Message(arbitration_id=0x20*dev_id, dlc=20, data= message_array, is_fd=True, extended_id=False)
+       
+        can0.send(msg_tx, 0.5)
+       
+        msg_rx = can0.recv()
+        print( msg_rx.data )
+        f1, f2, f3 = struct.unpack('3f', msg_rx.data)
+       
+        print (msg_rx)
+        print ( f1, f2, f3)
+       
+        time.sleep(0.3)
+ 
+        #can0.send(msg_tx, 0.5)
+       
+def main1(params):
+    os.system('sudo ifconfig can0 down')
+    os.system('sudo ip link set can0 up type can bitrate 1000000   dbitrate 1000000 restart-ms 1000 loopback on berr-reporting on fd on')
+   
+    can0 = can.interface.Bus(channel = 'can0', bustype='socketcan_ctypes', bitrate=1000000, data_bitrate=8000000, fd=True)
+   
+    # so much Indian code
+    if type(params[1]) == float:
+        message_array = bytearray(struct.pack('Lf', *params))
+    else:
+        message_array = bytearray(struct.pack('LL', *params))
+       
+    msg_tx = can.Message(arbitration_id= 0x20*dev_id + 3, dlc=8, data= message_array, is_fd=True, extended_id=False)
+   
+    can0.send(msg_tx, 0.5)
+   
+    while 1:
+           
+        msg_rx = can0.recv()
+        f1, f2, f3 = struct.unpack('3f', msg_rx.data)
+       
+        print (msg_rx)
+        print ( f1, f2, f3)
+       
+def main2(params):
+    os.system('sudo ifconfig can0 down')
+    os.system('sudo ip link set can0 up type can bitrate 1000000   dbitrate 1000000 restart-ms 1000 loopback on berr-reporting on fd on')
+   
+    can0 = can.interface.Bus(channel = 'can0', bustype='socketcan_ctypes', bitrate=1000000, data_bitrate=8000000, fd=True)
+       
+    message_array = bytearray(struct.pack('L', *params))
+   
+    msg_tx = can.Message(arbitration_id= 0x20*dev_id + 7, dlc=4, data= message_array, is_fd=True, extended_id=False)
+   
+    can0.send(msg_tx, 0.5)
+   
+    while 1:
+        msg_rx = can0.recv()
+        print (msg_rx)
+         
+def int_float(value):
+    try:
+        return np.array(value, dtype=np.int32).tobytes('C')#int(value, 0) # handle hex int format
+    except:
+        return np.array(value, dtype=np.float32).tobytes('C') # or just float
+   
+def main3(params):
+    byte1 = params[0]
+    byte2 = params[1]
+   
+    print( byte1, byte2)
+ 
+    arr = bytearray(int_float(np.delete(params, [0,1])))
+    print([ "0x%02x" % b for b in arr ])
+    print(len(arr))
+ 
+    os.system('sudo ifconfig can0 down')
+    os.system('sudo ip link set can0 up type can bitrate 1000000   dbitrate 1000000 restart-ms 1000 loopback on berr-reporting on fd on')
+   
+    can0 = can.interface.Bus(channel = 'can0', bustype='socketcan_ctypes', bitrate=1000000, data_bitrate=8000000, fd=True)
+       
+    message_array = arr
+    msg_tx = can.Message(arbitration_id= 0x20*dev_id + 7, dlc=len(arr)+2, data= message_array, is_fd=True, extended_id=False)
+   
+    can0.send(msg_tx, 0.5)
+   
+    while 1:
+        msg_rx = can0.recv()
+        print (msg_rx)
